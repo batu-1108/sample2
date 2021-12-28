6e6eb6a22e4375e439db1d012623626c077ab782
batu-1108 <93242382+batu-1108@users.noreply.github.com>
2021-12-28 16:08:33 +0900

    Add files via upload

---
+++ js/slideshow.js
@@ -0,0 +1,25 @@
+﻿$(function() {
+    $('.hamburger').click(function() {
+        $(this).toggleClass('active');
+ 
+        if ($(this).hasClass('active')) {
+            $('.globalMenuSp').addClass('active');
+        } else {
+            $('.globalMenuSp').removeClass('active');
+        }
+    });
+});
+
+jQuery(function ($) {
+    var fadeIn = $('.fade-in');
+    $(window).scroll(function () {
+      $(fadeIn).each(function () {
+        var offset = $(this).offset().top;
+        var scroll = $(window).scrollTop(); 
+        var windowHeight = $(window).height();
+        if (scroll > offset - windowHeight + 150) {
+          $(this).addClass("scroll-in");
+        }
+      });
+    });
+  });
